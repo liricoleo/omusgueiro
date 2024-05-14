b5f2f365c8e072d35e07fd4de8f1fd6839b0caa6
liricoleo <146575148+liricoleo@users.noreply.github.com>
2024-05-14 19:48:36 -0300

    Add files via upload
    Signed-off-by: liricoleo <146575148+liricoleo@users.noreply.github.com>

---
+++ sub6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+    seu nome: <input type="text" method ="POST">
+    <input type="submit" value="ENVIAR">
+
+    <?php
+
+
+
+    ?>
+
+    </body>
+</html>
